9b9dde9e0a29f23347b966da6ff8a4178d19291a
Daniel Greenheck <dgreenheck@gmail.com>
2020-01-03 00:24:30 -0800

    Setup skeleton view controllers and UI flow

---
+++ ImageStorageClient/ImageStorageClient/Views/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageStorageClient
+//
+//  Created by Daniel Greenheck on 1/2/20.
+//  Copyright © 2020 Daniel Greenheck. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
